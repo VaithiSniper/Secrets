664b8aa48ae4e681ee7b638c0ca78611b5347bab
Vaithee <vaithi.genghiskhan>
2021-06-06 14:28:15 +0530

    Using mongoose encryption

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
+const mongodb = require('mongodb')
+const encrypt = require('mongoose-encryption')
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.urlencoded({
@@ -9,11 +12,20 @@ app.use(express.urlencoded({
 }));
 //Module related
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/secretsDB",{useNewUrlParser:true,useUnifiedTopology:true})
-const userSchema = mongoose.Schema({
+mongoose.connect("mongodb://localhost:27017/secretsDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const Schema = mongoose.Schema
+const userSchema = new Schema({
     email: String,
     password: String
 })
+
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ['password']
+});
 const User = mongoose.model("user", userSchema)
 //Mongoose related
 
@@ -28,16 +40,14 @@ app.get('/register', function (req, res) {
 })
 app.post('/login', function (req, res) {
     User.find({
-    email : req.body.email,
-    password : req.body.password
-    },function(err,result)
-    {
-        if(!err)
-        {
-            if(!result)
-            res.send("<h1>Auth FAILED, TRY AGAIN</h1>")
+        email: req.body.email,
+        password: req.body.password
+    }, function (err, result) {
+        if (!err) {
+            if (!result)
+                res.send("<h1>Auth FAILED, TRY AGAIN</h1>")
             else
-            res.redirect("/")
+                res.redirect("/")
         }
     })
 })
@@ -46,13 +56,9 @@ app.post('/register', function (req, res) {
         email: req.body.email,
         password: req.body.password
     })
-    User.insertMany([newUser],function(err)
-    {
-    if(err)
-    console.log(err)
-    else
+  
+    newUser.save();
     res.redirect("/")
-    })
 })
 app.listen('3000', function (req, res) {
     console.log("Server up and running")
